8f74d6e388aa4a280bbb1978b9d7cff153969eab
awaqar <azzumwaqar@gmail.com>
2017-08-02 21:26:14 +0100

    Added Operator class with PlusOperator class

---
+++ app/src/main/java/com/example/macintosh/calculator/Operator.java
@@ -0,0 +1,9 @@
+package com.example.macintosh.calculator;
+
+/**
+ * Created by macintosh on 02/08/2017.
+ */
+
+public abstract class Operator {
+    public abstract int evaluate(int a, int b);
+}
---
+++ app/src/main/java/com/example/macintosh/calculator/PlusOperator.java
@@ -0,0 +1,13 @@
+package com.example.macintosh.calculator;
+
+/**
+ * Created by macintosh on 02/08/2017.
+ */
+
+public class PlusOperator extends Operator {
+
+    @Override
+    public int evaluate(int a, int b) {
+        return 0;
+    }
+}
